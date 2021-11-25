c39b81181c6ea030b1dbe6fc9a3b6138681ac456
Josué M. Sehnem <josue@sehnem.com>
2021-11-25 16:17:54 -0500

    fix config

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name='target-woocommerce',
-    version='1.0.0',
+    version='1.0.1',
     description='hotglue target for exporting data to woocommerce API',
     author='hotglue',
     url='https://hotglue.xyz',
