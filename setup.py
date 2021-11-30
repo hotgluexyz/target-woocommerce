1d8bb919fba33b8b2eb79cc2d2bea6f67395c20d
Josué M. Sehnem <josue@sehnem.com>
2021-11-30 14:06:20 -0500

    fix version number

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name='target-woocommerce',
-    version='1.0.4',
+    version='1.0.3',
     description='hotglue target for exporting data to woocommerce API',
     author='hotglue',
     url='https://hotglue.xyz',
